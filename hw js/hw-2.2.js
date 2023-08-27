5a1117a3d7bb10f3255066ed31c1a83485c82f9a
KitLeonov <nikita_leonov_1999@mail.ru>
2023-08-27 20:24:58 +0300

    Задачи по js на проверку

--- hw js/hw-2.2.js
+++ hw js/hw-2.2.js
@@ -1,2 +1,120 @@
+/*
+задание 1.1
+let a = Number(10);
+alert(a);
+
+задание 1.2
 let a = Number(20);
 alert(a);
+
+задание 2
+let releaseDate = String('2007');
+alert(`Год выпуска первого Iphone ${releaseDate}`);
+
+задание 3
+let creatorName = String('Брендан Эйх');
+alert(`Имя создателя Java Script ${creatorName}`);
+
+задание 4
+let a = 10;
+let b = 2;
+alert(`
+${a + b}
+${a - b}
+${a * b}
+${a / b}
+`);
+
+задание 5
+a = 2;
+b = 5;
+let result = Number(a ** b);
+alert(`${result}`);
+
+задание 6
+a = 9;
+b = 2;
+let divisionRemainder = Number(a % b);
+alert(`Остаток от деления ${divisionRemainder}`);
+
+задание 7
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num++;
+num = num--;
+alert(num);
+
+задание 8
+let age = prompt("Сколько вам лет?");
+alert(`Вам ${age} лет`);
+
+задание 9.0
+const user = {
+    name: 'Nikita',
+    age: 24,
+    isAdmin: false,
+}
+
+задание 9.1
+const user = {
+    name: 'Nikita',
+    age: 24,
+    isAdmin: false,
+}
+
+user['city of residence'] = String('Moscow');
+
+задание 9.2
+const user = {
+    name: 'Nikita',
+    age: 24,
+    isAdmin: false,
+}
+
+user['city of residence'] = String('Moscow');
+
+user['age'] = Number('20');
+
+задание 9.3
+const user = {
+    name: 'Nikita',
+    age: 24,
+    isAdmin: false,
+}
+
+user['city of residence'] = String('Moscow');
+
+user['age'] = Number('20');
+
+delete user['city of residence'];
+
+задание 9.4
+const user = {
+    name: 'Nikita',
+    age: 24,
+    isAdmin: false,
+}
+
+user['city of residence'] = String('Moscow');
+
+user['age'] = Number('20');
+
+delete user['city of residence'];
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user[info]);
+
+задание 10 
+
+let name = prompt("Введите ваше имя");
+alert(`Привет, ${name} !`);
+*/
+
+
+
+
+
