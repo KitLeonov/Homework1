2a58f4b0c7875b44a202cd82da43cf0e2f576e7d
KitLeonov <nikita_leonov_1999@mail.ru>
2023-09-09 07:07:51 +0300

    save

---
+++ hw js/hw-2.6.js
@@ -0,0 +1,85 @@
+/*
+exercise 1
+
+let arr = [1, 5, 4, 10, 0, 3];
+
+for (let elem of arr) {
+    console.log(elem);
+    if (elem === 10) {
+        break;
+    }
+}
+
+exercise 2
+
+let arr = [1, 5, 4, 10, 0, 3];
+
+console.log(arr.indexOf(4));
+
+
+exercise 3
+
+let arr = [1, 3, 5, 10, 20];
+
+console.log(arr.join([' ']));
+
+
+exercise 4
+
+
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = [];
+
+    for (let j = 0; j < 3; j++) {
+        arr[i].push(1);
+    }
+}
+
+console.log(arr);
+
+exercise 5
+
+let arr = [1, 1, 1];
+
+arr.push(2);
+arr.push(2);
+arr.push(2);
+
+console.log(arr);
+
+
+exercise 6
+
+let arr = [9, 8, 7, 'a', 6, 5];
+
+arr = arr.sort();
+
+let last = arr.pop();
+
+console.log(arr);
+
+
+exercise 7
+
+
+exercise 8
+
+let arr = 'abcdef';
+
+let arrSpl = arr.split('');
+
+arrSpl = arrSpl.reverse();
+
+arrSpl = arrSpl.join('');
+
+console.log(arrSpl);
+
+exercise 9
+
+
+
+*/
+
+
