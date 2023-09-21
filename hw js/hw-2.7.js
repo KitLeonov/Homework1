3ca9866d6fcf9f40512c0ec6091ef44edb91db50
KitLeonov <nikita_leonov_1999@mail.ru>
2023-09-21 21:13:29 +0300

    Исправленное

--- hw js/hw-2.7.js
+++ hw js/hw-2.7.js
@@ -1,71 +1,71 @@
 //exersice 1
 
-let greet = 'Привет, мир! ';
-greet = greet.toUpperCase();
-console.log(greet);
+// let greet = 'Привет, мир! ';
+// greet = greet.toUpperCase();
+// console.log(greet);
 
 //exersice 2
 
-function searchStart(array, string) {
-    return array.filter(function (item) {
-        return item.toLowerCase().startsWith(string.toLowerCase());
-    });
-}
+// function searchStart(array, string) {
+//     return array.filter(function (item) {
+//         return item.toLowerCase().startsWith(string.toLowerCase());
+//     });
+// }
 
-console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+// console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
 
 //exersice 3
 
-console.log(Math.floor(32.58884));
-console.log(Math.ceil(32.58884));
-console.log(Math.round(32.58884));
+// console.log(Math.floor(32.58884));
+// console.log(Math.ceil(32.58884));
+// console.log(Math.round(32.58884));
 
 //exersice 4
 
-const numbers = [52, 53, 49, 77, 21, 32];
-const maxNum = Math.max(...numbers);
-const minNum = Math.min(...numbers);
+// const numbers = [52, 53, 49, 77, 21, 32];
+// const maxNum = Math.max(...numbers);
+// const minNum = Math.min(...numbers);
 
-console.log(` Максимальное - ${maxNum}, минимальное - ${minNum}`);
+// console.log(` Максимальное - ${maxNum}, минимальное - ${minNum}`);
 
 //exersice 5
 
-const getRandomInt = (max, min) => {
-    return Math.round(Math.random() * (max - min)) + min;
-}
-console.log(getRandomInt(1, 10));
+// const getRandomInt = (max, min) => {
+//     return Math.round(Math.random() * (max - min)) + min;
+// }
+// console.log(getRandomInt(1, 10));
 
 //exersice 6
 
-function getRandomNumbers(num) {
-    let array = [];
-    for (i = 0; i < num / 2; i++) {
-        array.push(Math.floor(Math.random() * 10));
-    }
-    return array;
-}
-console.log(getRandomNumbers(10));
+// function getRandomNumbers(num) {
+//     let array = [];
+//     for (i = 0; i < Math.floor(num / 2); i++) {
+//         array.push(Math.floor(Math.random() * 10));
+//     }
+//     return array;
+// }
+// console.log(getRandomNumbers(7));
 
 //exersice 7 есть сомнения по данной задаче
 
-function getRandomInte(num1, num2) {
-    return Math.round(Math.random() * (num1 - num2)) + num2;
-}
-console.log(getRandomInte(1, 5));
+// function getRandomInte(num1, num2) {
+//     return Math.round(Math.random() * (num1 - num2)) + num2;
+// }
+// console.log(getRandomInte(1, 5));
 
 
 
 //exersice 8
 
-let myDate = new Date();
-console.log(myDate);
+// let myDate = new Date();
+// console.log(myDate);
 
 
 //exersice 9
 
-let currentDate = new Date();
-currentDate.setDate(currentDate.getDate() + 73);
-console.log(currentDate);
+// let currentDate = new Date();
+// currentDate.setDate(currentDate.getDate() + 73);
+// console.log(currentDate);
 
 
 //exersice 10
@@ -97,24 +97,24 @@ displayDate()
 //exersice 11
 
 
-function gameFruit() {
+// function gameFruit() {
 
-    let arrayFruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+//     let arrayFruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
 
-    arrayFruit = arrayFruit.sort(() => Math.random() - 0.5);
+//     arrayFruit = arrayFruit.sort(() => Math.random() - 0.5);
 
-    alert(arrayFruit);
+//     alert(arrayFruit);
 
-    let userFirstQuestion = prompt('Чему равняется первый элемент массива?').toLowerCase();
-    let userLastQuestion = prompt('Чему равняется последний элемент массива?').toLowerCase();
-    if (userFirstQuestion === arrayFruit[0].toLowerCase() && userLastQuestion === arrayFruit[arrayFruit.length - 1].toLowerCase()) {
-        alert('Поздравляю, вы победили!');
-    } else if (userFirstQuestion === arrayFruit[0].toLowerCase() || userLastQuestion === arrayFruit[arrayFruit.length - 1].toLowerCase()) {
-        alert('Вы были близки к победе!');
-    } else {
-        alert('Вы ответили неверно');
-    }
-}
-gameFruit();
+//     let userFirstQuestion = prompt('Чему равняется первый элемент массива?').toLowerCase();
+//     let userLastQuestion = prompt('Чему равняется последний элемент массива?').toLowerCase();
+//     if (userFirstQuestion === arrayFruit[0].toLowerCase() && userLastQuestion === arrayFruit[arrayFruit.length - 1].toLowerCase()) {
+//         alert('Поздравляю, вы победили!');
+//     } else if (userFirstQuestion === arrayFruit[0].toLowerCase() || userLastQuestion === arrayFruit[arrayFruit.length - 1].toLowerCase()) {
+//         alert('Вы были близки к победе!');
+//     } else {
+//         alert('Вы ответили неверно');
+//     }
+// }
+// gameFruit();
 
 
